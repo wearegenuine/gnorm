683fef0d2825d9c7b3a129e636d58bab4f772deb
Joe Magner <jmagner@genuineinteractive.com>
2016-02-12 12:48:50 -0500

    Initial commit with readme and blank generator

---
+++ app/index.js
@@ -0,0 +1,16 @@
+var generators = require('yeoman-generator');
+
+module.exports = generators.Base.extend({
+	prompting: function() {
+		this.log('what up');
+	},
+	writing: function() {
+		
+	},
+	install: function() {
+		
+	},
+	end: function() {
+		
+	}
+});
