d665c03ab9999afd827307555e7ba8186961dd50
Colby Cook <CCook@genuineinteractive.com>
2016-07-05 14:22:48 -0400

    Removed the self-invoking function from the componentReveal logic (not needed)

--- app/templates/app/scripts/modules/componentReveal/componentReveal.main.js
+++ app/templates/app/scripts/modules/componentReveal/componentReveal.main.js
@@ -4,28 +4,26 @@ var $ = require('jquery');
 
 module.exports = class ComponentReveal{
   constructor($el){
-		(function componentReveal(){
-			var host = window.location.host;
-			var hostCheck = function(urlTarget, hostToCheck){
-				var innerHost = hostToCheck.match(urlTarget);
-				if(innerHost !== null){
-					return true;
-				}
-			};
-			if(hostCheck('localhost', host) || hostCheck('phpstage', host)){
-				var search = window.location.search;
-				if(search !== ''){
-					search = search.split('?r=');
-					var revealTarget = $('' + search[1] + '');
-					if(revealTarget.length > 0){
-						var overlay = $('<div></div>', {
-							'class':'reveal-overlay'
-						}).appendTo('body');
-						$('body').addClass('reveal-adjust');
-						revealTarget.addClass('reveal');
-					}
+		var host = window.location.host;
+		var hostCheck = function(urlTarget, hostToCheck){
+			var innerHost = hostToCheck.match(urlTarget);
+			if(innerHost !== null){
+				return true;
+			}
+		};
+		if(hostCheck('localhost', host) || hostCheck('phpstage', host)){
+			var search = window.location.search;
+			if(search !== ''){
+				search = search.split('?r=');
+				var revealTarget = $('' + search[1] + '');
+				if(revealTarget.length > 0){
+					var overlay = $('<div></div>', {
+						'class':'reveal-overlay'
+					}).appendTo('body');
+					$('body').addClass('reveal-adjust');
+					revealTarget.addClass('reveal');
 				}
 			}
-		})();
+		}
   }
 };
