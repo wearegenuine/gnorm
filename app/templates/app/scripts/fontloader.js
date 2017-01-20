bd92e598e2dedbdfdae32c522e61692c8dc8b7c4
Ryan Brooks <RBrooks@wearegenuine.com>
2017-01-20 14:18:38 -0500

    Font Loader updates
    * Updated fontfaceobserver version
    * Updated the fontloader to use sessionStorage flag

--- app/templates/app/scripts/fontloader.js
+++ app/templates/app/scripts/fontloader.js
@@ -1,32 +1,32 @@
 'use strict';
 
-
 //This particular flavor also includes a tiny Promise shim for cross-browser compatibility
 var FontFaceObserver = require('fontfaceobserver/fontfaceobserver.js');
+var html = document.documentElement;
 
-
-var sansRegular = new FontFaceObserver('Roboto', {
-  weight: 400
+// Replace these with your project web fonts
+var normal = new FontFaceObserver('Roboto');
+var bold = new FontFaceObserver('Roboto', {
+  'font-weight': 700
 });
-var sansBold = new FontFaceObserver('Roboto', {
-  weight: 700
-});
 
+html.classList.add('fonts-loading');
 
 /* Should reference any and all custom Font Families being used in our so we
  * don't hide any text during the intial page load.
  */
 
 Promise.all([
-  sansRegular.check(),
-  sansBold.check()
-]).then(function() {
+  normal.load(),
+  bold.load()
+])
+.then(function() {
+  html.classList.remove('fonts-loading');
+  html.classList.add('fonts-loaded');
+  sessionStorage.fontsLoaded = true;
 
-  document.documentElement.className += ' ' + 'fonts-loaded';
-
-//Timeout fallback if something fails with the promises.
+// Timeout fallback if something fails with the promises.
 }, function() {
-
-  document.documentElement.className += ' ' + 'fonts-loaded';
-
+  html.classList.remove('fonts-loading');
+  html.classList.add('fonts-failed');
 });
