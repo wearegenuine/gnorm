357e93aa60d3aa7a42903cce2e8d315b9e5c3fa1
Ryan Brooks <RBrooks@wearegenuine.com>
2017-03-09 14:02:52 -0500

    Updated images task
    The default plugin that imagemin uses (imagemin-jpegtran) is lossless
    and does not reduce file size very much.  The images task now uses
    imagemin-mozjpeg plugin to compress jpegs at a quality of 80%.

--- app/templates/gnorm/tasks/images.js
+++ app/templates/gnorm/tasks/images.js
@@ -1,11 +1,17 @@
-var gulp       = require('gulp'),
-    changed    = require('gulp-changed'),
-    config     = require('../config').images,
-    imagemin   = require('gulp-imagemin');
+var gulp = require('gulp'),
+    changed = require('gulp-changed'),
+    config = require('../config').images,
+    imagemin = require('gulp-imagemin'),
+    imageminMozjpeg = require('imagemin-mozjpeg');
 
 gulp.task('images', function() {
   return gulp.src(config.src)
-    .pipe(changed(config.dest)) // Ignore unchanged files
-    .pipe(imagemin()) // Optimize
+    .pipe(changed(config.dest))
+    .pipe(imagemin([
+      imagemin.gifsicle(),
+      imageminMozjpeg({quality: 80}),
+      imagemin.optipng(),
+      imagemin.svgo()
+    ]))
     .pipe(gulp.dest(config.dest));
 });
