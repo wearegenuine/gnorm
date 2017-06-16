fb651b97a7e3df0c89b1c2e8afe5aa22bad986a3
Meghan Callahan <mcallahan@genuineinteractive.com>
2017-06-16 13:48:18 -0400

    re-add componentReveal to app.js

--- app/templates/app/scripts/app.js
+++ app/templates/app/scripts/app.js
@@ -2,5 +2,8 @@
 
 const $ = require('jquery')
 const moduleRegistry = require('./modules/')
+const ComponentReveal = require('./modules/componentReveal/componentReveal.main')
+
+const componentReveal = new ComponentReveal()
 
 moduleRegistry.init()
