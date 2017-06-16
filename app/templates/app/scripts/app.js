dc27041d6e1b80dcf3a5047cc734d3292792de13
Meghan Callahan <mcallahan@genuineinteractive.com>
2017-06-16 13:59:29 -0400

    ABC order

--- app/templates/app/scripts/app.js
+++ app/templates/app/scripts/app.js
@@ -1,8 +1,8 @@
 'use strict'
 
 const $ = require('jquery')
-const moduleRegistry = require('./modules/')
 const ComponentReveal = require('./modules/componentReveal/componentReveal.main')
+const moduleRegistry = require('./modules/')
 
 const componentReveal = new ComponentReveal()
 
