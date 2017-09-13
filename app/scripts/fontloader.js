770485b8fd90d181a70517a0f4e9e1f47b2cc977
Patrick Connors <pconnors@wearegenuine.com>
2017-09-13 09:41:54 -0400

    I missed a semicolon or several. Removing Modernizr from `head`. If needed on a project, it should be placed at the end of body, but before other JS.

--- app/scripts/fontloader.js
+++ app/scripts/fontloader.js
@@ -8,13 +8,11 @@ const bold = new FontFaceObserver('Roboto', {
   'font-weight': 700
 })
 
-html.classList.add('fonts-loading')
-
 // Should reference any and all custom Font Families being used in our so we
 // don't hide any text during the intial page load.
 
 if (!html.classList.contains('fonts-loaded')) {
-  html.classList.add('fonts-loading');
+  html.classList.add('fonts-loading')
 
   Promise.all([
     normal.load(),
@@ -24,9 +22,8 @@ if (!html.classList.contains('fonts-loaded')) {
       html.classList.remove('fonts-loading')
       html.classList.add('fonts-loaded')
       sessionStorage.fontsLoaded = true
-
-      // Timeout fallback if something fails with the promises.
     },
+    // Timeout fallback if something fails with the promises.
     function() {
       html.classList.remove('fonts-loading')
       html.classList.add('fonts-failed')
