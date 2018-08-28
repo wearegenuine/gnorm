95e96f9bf3cb5f008e8ea781768b0c3a10916cdb
Ryan Brooks <rbrooks@wearegenuine.com>
2018-08-28 17:55:36 -0400

    Cleanup webpack task

--- gnorm/tasks/webpack.js
+++ gnorm/tasks/webpack.js
@@ -2,50 +2,49 @@ const gulp = require('gulp'),
   log = require('fancy-log'),
   PluginError = require('plugin-error'),
   webpack = require('webpack'),
-  webpackConfig = require('../webpack.config');
-
-gulp.task('webpack', [])
-
+  webpackConfig = require('../webpack.config')
 
 // PRODUCTION
 gulp.task('webpack:build', function(callback) {
-  // modify some webpack config options
-  const myConfig = Object.assign(webpackConfig, {
+  const prodConfig = Object.assign(webpackConfig, {
     mode: 'production',
     optimization: {
       minimize: true
     }
   })
 
-  webpack(myConfig, function(err, stats) {
+  webpack(prodConfig, function(err, stats) {
     if (err) {
       throw new PluginError('webpack:build', err)
     }
+
     log('[webpack:build]', stats.toString({
       colors: true
     }))
+
     callback()
   })
 })
 
 
 // DEVELOPMENT
-// modify some webpack config options
-let myDevConfig = Object.assign(webpackConfig, {
-  mode: 'development',
-  optimization: {
-    minimize: false
-  }
-})
-
 gulp.task('webpack:build-dev', function(callback) {
-  // run webpack
-  webpack(myDevConfig, function(err, stats) {
-    if (err) throw new PluginError("webpack", err);
+  const devConfig = Object.assign(webpackConfig, {
+    mode: 'development',
+    optimization: {
+      minimize: false
+    }
+  })
+
+  webpack(devConfig, function(err, stats) {
+    if (err) {
+      throw new PluginError('webpack:build-dev', err)
+    }
 
     log("[webpack:build-dev]", stats.toString({
       colors: true
-    }));
-    callback();
-  });
+    }))
+
+    callback()
+  })
 })
